be538caeb2dbf3a41dbeb65a699ea06614d3d670
SunnyOhK <127900916+SunnyOhK@users.noreply.github.com>
2023-05-09 15:58:27 -0500

    starting to understand the JOIN within tables. Pushing before tutoring

--- db/query.sql
+++ db/query.sql
@@ -2,6 +2,44 @@ USE company_db;
 -- If the user chooses to update employee role, the role_id in employee table will change
 -- https://pencilprogrammer.com/foreign-key-in-mysql/
 
+-- ! How can I show the department name in the show all roles if the dept_id is listed in the roles table? ... same with employee variables
+--! var role.dept_id('value') = department.id(dept_name)
+-- WHAT IS A MANAGER?
+SELECT employee.id, employee.first_name, employee.last_name AS manager_name
+FROM employee
+LEFT JOIN employee AS manager ON employee.manager_id = manager.id AND manager.manager_id IS NULL;
+
+-- VIEW ALL DEPARTMENTS (id, dept_name)
+SELECT * FROM department
+ORDER BY dept_name;
+
+-- VIEW ALL ROLES (id, title, department, salary)
+SELECT 
+  role.id AS id, 
+  role.title AS title, 
+  department.dept_name AS department, 
+  role.salary AS salary
+FROM role
+LEFT JOIN department ON role.dept_id = department.id;
+ORDER BY role.id;
+
+
+-- VIEW ALL EMPLOYEES (id, first name, last name, title, department, salary, manager)
+SELECT 
+  employee.id AS id, 
+  employee.first_name AS firstName, 
+  employee.last_name AS lastName, 
+  role.title AS title, 
+  department.dept_name AS department, 
+  role.salary AS salary, 
+  CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+FROM employee
+LEFT JOIN role ON employee.role_id = role.id
+LEFT JOIN department ON role.dept_id = department.id
+LEFT JOIN employee AS manager ON employee.manager_id = manager.id AND manager.manager_id IS NULL
+ORDER BY id;
+
+-- UPDATE THE ROLE FOR AN EMPLOYEE
 UPDATE employee
 SET role_id = {new_role_id}
 WHERE id = {employee_id};
--- db/schema.sql
+++ db/schema.sql
@@ -4,17 +4,20 @@ CREATE DATABASE company_db;
 USE company_db;
 
 CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  dept_name VARCHAR(30) NOT NULL
+  id INT NOT NULL AUTO_INCREMENT,
+  dept_name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
   dept_id: INT NOT NULL,
+  PRIMARY KEY (id)
   FOREIGN KEY (dept_id)
   REFERENCES department(id)
+  ON DELETE SET NULL
   );
   
 CREATE TABLE employee (
@@ -23,8 +26,10 @@ CREATE TABLE employee (
   last_name VARCHAR(30) NOT NULL,
   role_id INT,
   manager_id INT,
+  PRIMARY (id)
   FOREIGN KEY (role_id)
     REFERENCES role(id),
   FOREIGN KEY (manager_id)
     REFERENCES employee(id)
+  ON DELETE SET NULL
 );
